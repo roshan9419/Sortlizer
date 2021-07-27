07dcd42c8ccf8eb662c80902e38a8f683cdd6258
Roshan <roshank9419@gmail.com>
2021-07-27 11:39:14 +0530

    HeapSort Code

--- lib/utils/contents.dart
+++ lib/utils/contents.dart
@@ -594,12 +594,60 @@ int main() {
   print(arr, n);
   
   return 0;
-}    
-}    
+}
     """;
   }
 
   String heapSortCode() {
-    return """""";
+    return """
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapSort(int arr[], int n) {
+  for (int i = n / 2; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+  
+  for (int i = n - 1; i >= 0; i--) {
+    swap(arr[0], arr[i]);
+    heapify(arr, i, 0);
+  }
+}
+
+void heapify(int arr[], int n, int i) {
+  
+  int largest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
+  
+  if (l < n && arr[l] > arr[largest]) largest = l;
+  if (r < n && arr[r] > arr[largest]) largest = r;
+  
+  if (largest != i) {
+    swap(arr[i], arr[largest]);
+    heapify(arr, n, largest);
+  }
+}
+  
+void print(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+}
+
+int main() {
+  int arr[] = {10, 20, 10, 90, 30, 80, 70, 45};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  
+  cout << "Before Sorting: ";
+  print(arr, n);
+  
+  heapSort(arr, n);
+  
+  cout << "\\nAfter Sorting: ";
+  print(arr, n);
+  
+  return 0;
+}
+    """;
   }
 }
